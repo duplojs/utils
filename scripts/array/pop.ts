585b1568029e226329d37442508796c4ced6a722
mathcovax <campani.mathieu@gmail.com>
2025-09-08 11:41:45 +0000

    fix: pop

--- scripts/array/pop.ts
+++ scripts/array/pop.ts
@@ -1,11 +1,9 @@
-type Pop<
-	GenericArray extends readonly unknown[],
-> = GenericArray extends readonly [...infer Rest, any]
-	? Rest
-	: GenericArray;
+import { type AnyTuple, type PopTuple } from "@scripts/common";
 
 export function pop<
 	const GenericArray extends readonly unknown[],
->(array: GenericArray): Pop<GenericArray> {
+>(array: GenericArray): GenericArray extends AnyTuple
+	? PopTuple<GenericArray>
+	: GenericArray {
 	return array.slice(0, -1) as never;
 }
