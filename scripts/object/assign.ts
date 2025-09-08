18c828755385956e6fc4892044c2e0576ade0889
mathcovax <campani.mathieu@gmail.com>
2025-09-08 12:24:51 +0000

    feat: pipe object assign

--- scripts/object/assign.ts
+++ scripts/object/assign.ts
@@ -5,9 +5,26 @@ export function assign<
 	GenericInput extends object,
 	GenericValue extends Partial<Record<keyof GenericInput, unknown>> & AnyObject,
 >(
+	value: GenericValue,
+): (input: GenericInput) => AssignObjects<GenericInput, GenericValue>;
+
+export function assign<
+	GenericInput extends object,
+	GenericValue extends Partial<Record<keyof GenericInput, unknown>> & AnyObject,
+>(
 	input: GenericInput,
 	value: GenericValue,
-): AssignObjects<GenericInput, GenericValue> {
+): AssignObjects<GenericInput, GenericValue>;
+
+export function assign(...args: [object, object] | [object]) {
+	if (args.length === 1) {
+		const [value] = args;
+
+		return (input: object) => assign(input, value);
+	}
+
+	const [input, value] = args;
+
 	return {
 		...input,
 		...value,
