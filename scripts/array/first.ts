ef1f52fe73750a4acc5f8eec107e31ff334751ee
mathcovax <campani.mathieu@gmail.com>
2025-09-08 11:40:19 +0000

    fix: first

--- scripts/array/first.ts
+++ scripts/array/first.ts
@@ -1,9 +1,11 @@
+import { type AnyTuple } from "@scripts/common";
+
 export function first<
 	GenericArray extends readonly unknown[],
 >(
 	array: GenericArray,
-): GenericArray extends readonly [infer InferredValue, ...any[]]
-		? InferredValue
+): GenericArray extends AnyTuple
+		? GenericArray[0]
 		: GenericArray[number] | undefined {
 	return array[0] as never;
 }
