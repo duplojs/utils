d22024251e9141b965242bc0d1fabe17054c115c
mathcovax <campani.mathieu@gmail.com>
2025-09-21 16:13:50 +0000

    fix: pattern whenPrimitive

--- scripts/pattern/whenPrimitive.ts
+++ scripts/pattern/whenPrimitive.ts
@@ -1,5 +1,5 @@
 import { type AnyFunction, type AnyValue } from "..";
-import { result, type PatternResult } from "./result";
+import { isResult, result, type PatternResult } from "./result";
 
 export function whenPrimitive<
 	GenericInput extends AnyValue,
@@ -12,40 +12,19 @@ export function whenPrimitive<
 	| PatternResult<GenericOutput>
 	| Exclude<GenericInput, GenericMatch>;
 
-export function whenPrimitive<
-	GenericInput extends AnyValue,
-	GenericMatch extends Exclude<GenericInput, PatternResult>,
-	GenericOutput extends AnyValue,
->(
-	input: GenericInput,
-	primitive: GenericMatch | GenericMatch[],
-	theFunction: (predicatedInput: Extract<GenericInput, GenericMatch>) => GenericOutput
-):
-	| PatternResult<GenericOutput>
-	| Exclude<GenericInput, GenericMatch>;
-
 export function whenPrimitive(
-	...args: [AnyValue, AnyValue, AnyFunction] | [ AnyValue, AnyFunction]
+	primitive: AnyValue,
+	theFunction: AnyFunction,
 ): any {
-	if (args.length === 2) {
-		const [primitive, theFunction] = args;
-
-		return (input: AnyValue) => whenPrimitive(
-			input,
-			primitive as never,
-			theFunction as never,
-		);
-	}
-
-	const [input, primitive, theFunction] = args;
-
 	const formattedPrimitive = primitive instanceof Array
 		? primitive
 		: [primitive];
 
-	if (formattedPrimitive.includes(input)) {
-		return result(theFunction(input));
-	}
+	return (input: AnyValue) => {
+		if (!isResult(input) && formattedPrimitive.includes(input)) {
+			return result(theFunction(input));
+		}
 
-	return input;
+		return input;
+	};
 }
--- tests/pattern/whenPrimitive.test.ts
+++ tests/pattern/whenPrimitive.test.ts
@@ -2,52 +2,6 @@ import { pipe, type ExpectType } from "@scripts/common";
 import { DPattern } from "@scripts/index";
 
 describe("pattern whenPrimitive", () => {
-	it("match", () => {
-		const matchInput = "car" as 10 | "car" | "train";
-
-		const matched = DPattern.whenPrimitive(
-			matchInput,
-			["car", "train"],
-			(value) => {
-				type checkValue = ExpectType<
-					typeof value,
-					"car" | "train",
-					"strict"
-				>;
-
-				return 150;
-			},
-		);
-
-		expect(matched).toStrictEqual(DPattern.result(150));
-
-		type checkMatched = ExpectType<
-			typeof matched,
-			10 | DPattern.PatternResult<150>,
-			"strict"
-		>;
-	});
-
-	it("not match", () => {
-		const matchInput = 10 as 10 | "car" | "train";
-
-		const matched = DPattern.whenPrimitive(
-			matchInput,
-			["car", "train"],
-			(value) => {
-				type checkValue = ExpectType<
-					typeof value,
-					"car" | "train",
-					"strict"
-				>;
-
-				return 150;
-			},
-		);
-
-		expect(matched).toStrictEqual(10);
-	});
-
 	it("use in pipe", () => {
 		const result = pipe(
 			10 as 10 | "car" | "train",
