be6141065cdef047af6cf99690cd77b640649aca
ZeRiix <willflo46@gmail.com>
2025-10-13 16:51:33 +0200

    feat(19): continue api string

--- docs/v1/api/string/split/examples/splitUrl.doc.ts
+++ docs/v1/api/string/split/examples/splitUrl.doc.ts
@@ -1,10 +1,12 @@
-import { DArray, DString, pipe } from "@duplojs/utils";
+import { DArray, DString, pipe, not, or, equal } from "@duplojs/utils";
 
 const input = "https://api.example.com/v1/users/42/posts";
 const result = pipe(
 	input,
 	DString.split("/"),
-	DArray.filter((value) => !!value && !DString.startsWith(value, "http")),
+	DArray.filter(
+		not(or([equal(""), equal("https:")])),
+	),
 	DArray.slice(2),
 );
 // result: ["api.example.com", "v1", "users", "42", "posts"]
