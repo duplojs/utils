bbd3aa06776b867abc1acef404300b90be65d7c1
mathcovax <campani.mathieu@gmail.com>
2025-11-14 09:34:42 +0000

    fix(hf): dstring replace with function

--- tests/string/replace.test.ts
+++ tests/string/replace.test.ts
@@ -5,6 +5,52 @@ describe("replace", () => {
 		expect(DString.replace("javascript", /script/, "lang")).toBe("javalang");
 	});
 
+	it("should behave like native replace when using a function replacer", () => {
+		const input = "Hello, ${first} ${last}! Welcome back, ${first}.";
+		const pattern = /\${(\w+)}/g;
+
+		const nativeResult = input.replace(
+			pattern,
+			(
+				match: string,
+				variableName: string,
+				offset: number,
+				self: string,
+			) => `${variableName.toUpperCase()}@${offset}@${self.length}`,
+		);
+		const duploResult = DString.replace(
+			input,
+			pattern,
+			({ matchedValue, groups, offset, self }) => `${groups[0]!.toUpperCase()}@${offset}@${self.length}`,
+		);
+
+		expect(duploResult).toBe(nativeResult);
+		expect(
+			DString.replace(
+				input,
+				"9",
+				() => "8",
+			),
+		).toBe(input);
+	});
+
+	it("test named group", () => {
+		const input = "Hello, ${first} ${last}! Welcome back, ${first}.";
+		const pattern = /\${(?<myG>\w+)}/g;
+
+		const nativeResult = input.replace(
+			pattern,
+			(...args) => JSON.stringify(args.pop()),
+		);
+		const duploResult = DString.replace(
+			input,
+			pattern,
+			({ namedGroups }) => JSON.stringify(namedGroups),
+		);
+
+		expect(duploResult).toBe(nativeResult);
+	});
+
 	it("use in pipe", () => {
 		const result = pipe(
 			["duplojs", "nestjs", "expressjs"],
