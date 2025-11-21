edf58f9430163cdbf051cba17a72f7316fbe65d4
ZeRiix <willflo46@gmail.com>
2025-11-21 17:21:24 +0100

    feat(19): add dataParser

--- docs/v1/fastExample.doc.ts
+++ docs/v1/fastExample.doc.ts
@@ -1,12 +1,17 @@
-import { pipe, DArray, DNumber } from "@duplojs/utils";
+import { pipe, DArray, DNumber, equal, innerPipe } from "@duplojs/utils";
 
 const result = pipe(
 	[1, 2, 3, 4, 5, 6],
-	DArray.filter((value) => value % 2 === 0),
-	DArray.map((value) => value * 2),
+	DArray.filter(
+		innerPipe(
+			DNumber.modulo(2),
+			equal(0),
+		),
+	),
+	DArray.map(DNumber.multiply(2)),
 	DArray.reduce(
 		0,
-		({ element, lastValue, next }) => next(element + lastValue),
+		({ element, lastValue, next }) => next(DNumber.add(element, lastValue)),
 	),
 	DNumber.round,
 );
